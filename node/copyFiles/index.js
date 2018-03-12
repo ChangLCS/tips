37bcb81f9efe8d3ef2fa0592708841c51f9fde51
Chang <584911924@qq.com>
2018-03-12 23:43:45 +0800

    node测试加学习

---
+++ node/copyFiles/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const fs = require('fs'); //  操作文件或文件流
+const path = require('path'); //  操作路径
+
+// function copy(src, dst) {
+//   fs.writeFileSync(dst, fs.readFileSync(src));
+// }
+
+// function main(argv) {
+//   copy(argv[0], argv[1]);
+// }
+
+// main(process.argv.slice(2));
