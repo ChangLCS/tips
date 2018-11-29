290eef3232c0216e5b1f552c7c67f0e7e9d6d6cc
Chang <584911924@qq.com>
2018-11-29 22:47:57 +0800

    调整一下修改音乐修改名称

--- node/rename/index.js
+++ node/rename/index.js
@@ -15,11 +15,11 @@ const reg = new RegExp(/(.*)-(.*).mp3/);
 console.log('arr', arr);
 for (let i = 0; i < arr.length; i += 1) {
   const item = arr[i];
-  if (!reg.test(item)) {
-    const str = item.replace(/…|在播放|正在播放|…正/g, '');
-    const res = str.replace(reg, '$2 -$1.mp3');
-    fs.renameSync(path.resolve(src, item), path.resolve(src, res));
-  }
+  // if (!reg.test(item)) {
+  const str = item.replace(/…|在播放|正在播放|…正/g, '');
+  const res = str.replace(reg, '$2 -$1.mp3');
+  fs.renameSync(path.resolve(src, item), path.resolve(src, res));
+  // }
 }
 const list = fs.readdirSync(src);
 console.log('list', list);
