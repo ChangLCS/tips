527a5273a18c476e0e8e1306b15cf393aae360fb
Chang <584911924@qq.com>
2019-11-25 17:57:30 +0800

    抓取http://datav.aliyun.com/tools/atlas/GEO地图

---
+++ getDatavGeo/api.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+const instance = axios.create({
+  baseURL: 'https://geo.datav.aliyun.com/areas/bound/',
+  withCredentials: true,
+  params: {},
+  timeout: 0,
+});
+
+instance.interceptors.request.use(
+  (request) => {
+    const axiosRequest = {
+      ...request,
+      params: {},
+    };
+    return axiosRequest;
+  },
+  (error) => {
+    // console.error(error);
+    return Promise.reject(error);
+  },
+);
+
+instance.interceptors.response.use(
+  (response) => {
+    // console.log('response', response);
+    return response;
+  },
+  (error) => {
+    // console.error(error);
+    return Promise.reject(error);
+  },
+);
+module.exports = instance;
---
+++ getDatavGeo/index.js
@@ -0,0 +1,60 @@
+const path = require('path');
+const fs = require('fs');
+
+const api = require('./api');
+const codeJSON = require('./code');
+
+const data = Object.entries(codeJSON);
+let index = 0;
+const timeoutTime = 100;
+
+const success = (base, ret, callback) => {
+  const filePath = path.resolve(__dirname, '../lib/geo', `${base[0]}-${base[1]}.json`);
+  const filePathFull = path.resolve(__dirname, '../lib/geo', `${base[0]}_full-${base[1]}.json`);
+  const newFile = fs.createWriteStream(filePathFull);
+
+  newFile.write(JSON.stringify(ret), 'UTF8');
+  newFile.end();
+
+  newFile.on('finish', () => {
+    setTimeout(() => {
+      console.log('success', index, filePathFull);
+      callback();
+    }, timeoutTime);
+  });
+};
+
+const apiGet = () => {
+  const end = () => {
+    if (index < data.length) {
+      index += 1;
+      apiGet();
+    }
+  };
+
+  const base = data[index];
+  if (!base) {
+    console.log('结束了', index);
+    return;
+  }
+  const url = `${base[0]}.json`;
+  const urlFull = `${base[0]}_full.json`;
+  api
+    .get(urlFull)
+    .then((res) => {
+      console.log('res', res.data);
+      const ret = res.data;
+      success(base, ret, () => {
+        end();
+      });
+    })
+    .catch((res) => {
+      // console.error('error', res);
+      console.error('error', index, urlFull, res.message);
+      setTimeout(() => {
+        end();
+      }, timeoutTime);
+    });
+};
+
+apiGet();
