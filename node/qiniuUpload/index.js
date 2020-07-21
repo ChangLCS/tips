6c9f7f20666d82121d432738b871aba7a7695586
ChangLCS <584911924@qq.com>
2020-07-21 22:52:43 +0800

    七牛云上传新增修改名称

--- node/qiniuUpload/index.js
+++ node/qiniuUpload/index.js
@@ -30,8 +30,44 @@ const uploadFile = (key, localFile) =>
       } else {
         reject(respBody);
       }
-      6;
     });
+  });
+
+/**
+ * 修改名称
+ * srcKey 原位置
+ * destKey 修改后的位置
+ */
+const renameFile = (srcKey, destKey) =>
+  new Promise((resolve, reject) => {
+    const mac = new qiniu.auth.digest.Mac(config.accessKey, config.secretKey);
+    const qiniuConfig = new qiniu.conf.Config();
+    qiniuConfig.zone = qiniu.zone.Zone_z2;
+    const bucketManager = new qiniu.rs.BucketManager(mac, config);
+
+    // 强制覆盖已有同名文件
+    const options = {
+      force: true,
+    };
+
+    console.log('srcKey', srcKey, 'destKey', destKey);
+    bucketManager.move(
+      config.bucket,
+      srcKey,
+      config.bucket,
+      destKey,
+      options,
+      (respErr, respBody, respInfo) => {
+        if (respErr) {
+          reject(respErr);
+        }
+        if (respInfo.statusCode == 200) {
+          resolve(respBody);
+        } else {
+          reject(respBody);
+        }
+      },
+    );
   });
 
 const basePath = path.resolve(__dirname, '../../geoJSON');
@@ -39,7 +75,7 @@ console.log('basePath', basePath);
 
 const allFilePath = []; //  所有需要上传的文件
 
-const baseKey = '/geo/base';
+const baseKey = 'geo/base';
 const baseList = fs.readdirSync(path.resolve(basePath, `.${baseKey}`));
 baseList.map((item) => {
   const data = {
@@ -49,7 +85,7 @@ baseList.map((item) => {
   allFilePath.push(data);
 });
 
-const fullKey = '/geo/full';
+const fullKey = 'geo/full';
 const fullList = fs.readdirSync(path.resolve(basePath, `.${fullKey}`));
 fullList.map((item) => {
   const data = {
@@ -60,11 +96,21 @@ fullList.map((item) => {
 });
 
 (async () => {
+  const errorArr = [];
   for (let i = 0; i < allFilePath.length; i += 1) {
     const item = allFilePath[i];
-    await uploadFile(item.key, item.localFile);
-    console.log('完成1 ', i);
+    try {
+      await uploadFile(item.key, item.localFile);
+      // await renameFile(item.key, item.key);
+      console.log('完成1 ', i);
+    } catch (error) {
+      errorArr.push({
+        index: i,
+        error,
+      });
+      console.error(error.message);
+    }
   }
 
-  console.log('allFilePath', allFilePath.length, allFilePath);
+  console.log('errorArr', errorArr.length, errorArr);
 })();
