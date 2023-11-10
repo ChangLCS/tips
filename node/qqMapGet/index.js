b4891a0579f239609f790c5cc4843b3340287306
Chang <584911924@qq.com>
2023-11-10 14:21:25 +0800

    fix: 暂存地址获取，然后进行处理了

---
+++ node/qqMapGet/index.js
@@ -0,0 +1,88 @@
+const axios = require('axios');
+const fs = require('fs');
+const path = require('path');
+const addressJson = require('./libs/tms地址表.json');
+
+const base = { key: '6OXBZ-46NKF-BX3JZ-JNM34-53A3V-IJFIR' };
+
+const api = axios.create({
+  baseURL: 'https://apis.map.qq.com',
+});
+
+const apiPath = {
+  qqAddressToPoint: '/ws/geocoder/v1',
+};
+
+const retList = []; //  有效结果
+const errList = []; //  错误结果
+
+//  最终结果
+const endFn = () => {
+  const successSrc = path.resolve(__dirname, './libs/success.json');
+  const errorSrc = path.resolve(__dirname, './libs/error.json');
+
+  console.log('successSrc', successSrc);
+  console.log('errorSrc', errorSrc);
+
+  fs.writeFileSync(successSrc, JSON.stringify(retList));
+  fs.writeFileSync(errorSrc, JSON.stringify(errList));
+};
+
+//  传地址，获取ip
+const getQqAddressToPoint = (address) =>
+  api.get(apiPath.qqAddressToPoint, { params: { key: base.key, address } });
+
+const goGetAddress = (i, allData) => {
+  if (i >= 1) {
+    // if (i >= allData.length) {
+    endFn();
+    return;
+  }
+
+  const item = allData[i];
+
+  getQqAddressToPoint(item.address_info)
+    .then((res) => {
+      const data = res.data.result;
+      if (!data) {
+        errList.push({
+          ...item,
+          $message: res.data.message,
+        });
+        console.log('失败', item.customer_name, item.address_info, res.data.message);
+      } else {
+        const x = {
+          ...item,
+          lon: data.location.lng,
+          lat: data.location.lat,
+        };
+
+        console.log('成功', item.customer_name);
+        retList.push(x);
+      }
+
+      goGetAddress(i + 1, allData);
+    })
+    .catch((error) => {
+      console.log('失败', item.customer_name, item.address_info);
+      errList.push({
+        ...item,
+        $message: error.message,
+      });
+
+      goGetAddress(i + 1, allData);
+    });
+};
+
+//  数据处理
+const dataInit = () => {
+  const data = addressJson.RECORDS;
+  const filterData = data.filter((e) => e.address_info && !/~|\//.test(e.address_info));
+
+  const calcData = filterData.slice(0, 10000);
+  console.log('calcData,', calcData);
+
+  goGetAddress(0, calcData);
+};
+
+dataInit();
